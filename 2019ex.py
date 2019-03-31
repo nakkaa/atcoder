d2e1ddd8771af6b331b940e2367ef762c47445af
nakkaa <tnakkaa@gmail.com>
2019-03-31 12:25:27 +0900

    add 2019ex

---
+++ 2019ex.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+def ex2019a():
+    n = raw_input().split(" ")
+    a = int(n[0])
+    b = int(n[1])
+    c = int(n[2])
+
+    if a == b and b == c:
+        print "Yes"
+    else:
+        print "No"
+
+def ex2019b():
+    N = int(raw_input())
+    S = raw_input()
+    s = list(S)
+
+    r = 0
+    b = 0
+
+    for i in range(N):
+        if s[i] == "R":
+            r = r + 1
+        elif s[i] == "B":
+            b = b + 1
+    
+    if r > b:
+        print "Yes"
+    else:
+        print "No"
+
+def main():
+    ex2019b()
+	
+if __name__ == '__main__':
+    main()
